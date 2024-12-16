a340f95103c3574dbb52961a237adbd978636dca
Giorgio Giannotta <jorge_upa@msn.com>
2024-12-16 08:07:20 +0700

    Initial Commit

---
+++ BMI_CS4405/BMI_CS4405App.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_CS4405App.swift
+//  BMI_CS4405
+//
+//  Created by Giorgio Giannotta on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BMI_CS4405App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
